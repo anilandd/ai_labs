527b4cfe759f4bcb66c37e8d06634927f873a0b2
Anastasia Rudenko <aniland.send@gmail.com>
2020-03-15 22:01:26 +0200

    dirty code, but working. found best solution. need prettying

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -3,8 +3,7 @@ max_jar2 = 3
 goals = [[4,0],[4,3]]
 dead_end = [[0,0], [max_jar1,max_jar2]]
 cur_state = [0,0] # current state of jar1 and jar2
-branch = [cur_state]
-new_branch = [[]]
+# branch = [cur_state]
 history = [cur_state]
 
 def fill_j1(cur_state):
@@ -39,8 +38,8 @@ def from_j2_to_j1(cur_state):
         next_state = [cur_state[0] + dif1, cur_state[1] - dif1]
     return next_state    
 
-def util_add(cur_state):
-    if not(cur_state in branch) and not(cur_state in dead_end):
+def util_add(cur_state, branch):
+    if not(cur_state in branch) and not(cur_state in dead_end) and not(cur_state in history):
         branch.append(cur_state)
         print(branch)
         return cur_state
@@ -49,43 +48,54 @@ def util_add(cur_state):
 
 
 def search(cur_state):
-
+    branch = [cur_state]
+    rem = cur_state
     while not(cur_state in goals):
     
         if (cur_state[0] != max_jar1) and (cur_state[1] != max_jar2):
             cur_state = fill_j1(cur_state)
-            cur_state = util_add(cur_state)
+            cur_state = util_add(cur_state, branch)
 
         if (cur_state[0] != max_jar1) and (cur_state[1] != max_jar2):
             cur_state = fill_j2(cur_state)
-            cur_state = util_add(cur_state)
+            cur_state = util_add(cur_state, branch)
     
         if not(cur_state in dead_end) and ((cur_state[0] >= 0) and (cur_state[1] != 0)):
             cur_state = pour_j1(cur_state)
-            cur_state = util_add(cur_state)
+            cur_state = util_add(cur_state, branch)
 
         if not(cur_state in dead_end) and ((cur_state[1] >= 0) and (cur_state[0] != 0)):
             cur_state = pour_j2(cur_state)
-            cur_state = util_add(cur_state)
+            cur_state = util_add(cur_state, branch)
 
         if (cur_state != [0,0]) and (cur_state != [5,3]):
             if cur_state[0] != 0 and cur_state[1] != max_jar2:
                 cur_state = from_j1_to_j2(cur_state)
-                cur_state = util_add(cur_state)
+                cur_state = util_add(cur_state, branch)
             if cur_state[0] != max_jar1 and cur_state[1] != 0:
                 cur_state = from_j2_to_j1(cur_state)
-                cur_state = util_add(cur_state)
+                cur_state = util_add(cur_state, branch)
+        if cur_state == rem:
+            break
 
-    print("new solution: ", branch)
+    if branch != history:
+        print("new solution: ", branch)
 
     return branch
+
+def dfs ():
+    i = len(history) - 1 
+    while i >= 0:
+        new_branch = []
+        new_branch = search(history[i])
+        i -= 1
 
     
 history = search(cur_state)
 print("Best solution found: ", history, " - ", len(history) - 1, " steps found")
 print("-------------------------------------------------------------------------------------------------------------")
-new_branch  = search([2,3])
-# branch = [[]]
-# branch[0] = [2,3]
-# x = search(branch[0])
-# print(x)
+# branch = []
+# branch.append([2,3])
+# branch  = search(branch[0])
+dfs()
+
